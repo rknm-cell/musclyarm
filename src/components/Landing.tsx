083b6ca8fe5c6278ca09e596056cc049454e4a3c
rknm-cell <racknahm@gmail.com>
2024-07-18 22:17:05 -0400

    first commit

---
+++ src/components/Landing.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <>
+    <div>
+        <h1>Muscly Arm</h1>
+        <h2>exercise tracker </h2>
+    </div>
+    
+    </>
+  )
+}
+
+export default Landing
